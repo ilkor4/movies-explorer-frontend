e23fc2b619955a5f56e7f1a483ba9ae71989c40f
ilkor4 <kornelyuck.ilya@yandex.ru>
2023-06-24 14:28:03 +0300

    feat: Добавить логин и регистрацию

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,15 +3,15 @@ import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Movies from '../Movies/Movies';
 import Footer from '../Footer/Footer';
+import Register from '../Register/Register';
+import Login from '../Login/Login';
 import '../App/App.css';
 
 export default function App() {
   return(
     <div className='App'>
       <>
-        <Header />
-        <Movies />
-        <Footer />
+        <Login />
       </>
     </div>
   )
---
+++ src/components/Register/Register.js
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+
+import logoImage from '../../images/headerLogo.svg';
+import '../Register/Register.css';
+
+export default function Register() {
+  return(
+    <main className='register'>
+      <div className='register__container'>
+        <img src={logoImage} alt='Логотип сайта' className='register__logo'/>
+        <h1 className='register__title'>Добро пожаловать!</h1>
+        <form className='register__form'>
+          <legend className='form__title'>Имя</legend>
+          <input className='form__input' name="name" type="text" id="inputName" required minLength="2" maxLength="30"></input>
+          <legend className='form__title'>E-mail</legend>
+          <input className='form__input' name="email" type="email" id="inputEmail" required minLength="2" maxLength="30"></input>
+          <legend className='form__title'>Пароль</legend>
+          <input className='form__input form__input_type_password' name="password" type="password" id="inputPassword" required minLength="2" maxLength="30"></input>
+          <p className='form__text-error'>Что-то пошло не так...</p>
+          <button className="form__save-button" type="submit">Зарегистрироваться</button>
+          <p className="form__caption">Уже зарегистрированы? <Link to="/sign-in" className='form__link'>Войти</Link></p>
+        </form>
+      </div>
+    </main>
+  )
+}
